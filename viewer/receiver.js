6f732e7b23452d365be66b94be6d96bb11cb2150
june29 <june29x@gmail.com>
2010-08-17 10:42:37 +0900

    ラベルの変更に対応した

--- viewer/receiver.js
+++ viewer/receiver.js
@@ -57,7 +57,7 @@ $(document).ready(function() {
     }
   });
 
-  stream.bind("irc", function(message) {
+  stream.bind("irc-rubykaigi2010", function(message) {
     var data       = message.body;
     var nick       = data.nick;
     var text       = data.text;
